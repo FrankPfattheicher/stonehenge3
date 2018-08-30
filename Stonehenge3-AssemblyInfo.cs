e4af80ff7ac85ae23ca15b16351ea81c03cb83cc
Frank Pfattheicher <fpf@ict-baden.de>
2018-08-30 14:11:00 +0200

    Assembly version fixed

--- Stonehenge3-AssemblyInfo.cs
+++ Stonehenge3-AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyCopyrightAttribute("Copyright ©2013-2018 ICT Baden GmbH")]
 [assembly: AssemblyCompanyAttribute("ICT Baden GmbH")]
 [assembly: AssemblyProductAttribute("X-Platform & Web Application Framework")]
-[assembly: AssemblyVersionAttribute("3.0.9")]
-[assembly: AssemblyFileVersionAttribute("3.0.9")]
+[assembly: AssemblyVersionAttribute("3.0.11")]
+[assembly: AssemblyFileVersionAttribute("3.0.11")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyCopyright = "Copyright ©2013-2018 ICT Baden GmbH";
         internal const System.String AssemblyCompany = "ICT Baden GmbH";
         internal const System.String AssemblyProduct = "X-Platform & Web Application Framework";
-        internal const System.String AssemblyVersion = "3.0.9";
-        internal const System.String AssemblyFileVersion = "3.0.9";
+        internal const System.String AssemblyVersion = "3.0.11";
+        internal const System.String AssemblyFileVersion = "3.0.11";
     }
 }
