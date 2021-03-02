d8e737310aeb14f8f81719d96861e1e8667db9f4
Frank Pfattheicher <fpf@ict-baden.de>
2021-03-02 11:28:11 +0100

    Support in-proc and out-of-proc IIS hosting

--- IctBaden.Stonehenge3.Kestrel/WindowsHosting.cs
+++ IctBaden.Stonehenge3.Kestrel/WindowsHosting.cs
@@ -4,10 +4,18 @@ namespace IctBaden.Stonehenge3.Kestrel
 {
     public static class WindowsHosting
     {
+        /// <summary>
+        /// Host ASP.NET Core on Windows with IIS
+        /// https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/?view=aspnetcore-2.2#enable-the-iisintegration-components
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns>builder</returns>
         // ReSharper disable once InconsistentNaming
         public static IWebHostBuilder EnableIIS(IWebHostBuilder builder)
         {
-            return builder.UseIISIntegration();
+            return builder
+                .UseIIS()               // in-proc hosting
+                .UseIISIntegration();   // out-of-proc hosting   
         }
 
     }
