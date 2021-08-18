4706a0f82e284ca49f7e24d09912afe0dc6ee833
Frank Pfattheicher <fpf@ict-baden.de>
2021-08-18 18:35:20 +0200

    Added image and sound animation sample

---
+++ IctBaden.Stonehenge3.Vue.SampleCore/ViewModels/ImagesVm.cs
@@ -0,0 +1,35 @@
+﻿using IctBaden.Stonehenge3.Core;
+using IctBaden.Stonehenge3.ViewModel;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace IctBaden.Stonehenge3.Vue.SampleCore.ViewModels
+{
+    public class ImagesVm : ActiveViewModel
+    {
+        public bool IsOn { get; set; }
+
+        public string SwitchImg => IsOn 
+            ? "images/switch_on.png" 
+            : "images/switch_off.png";
+
+        public string LampImg => IsOn 
+            ? "images/lightbulb_on.png" 
+            : "images/lightbulb.png";
+
+        public ImagesVm(AppSession session)
+            : base(session)
+        {
+        }
+
+        [ActionMethod]
+        public void Switch()
+        {
+            IsOn = !IsOn;
+            
+            ExecuteClientScript(IsOn 
+                ? "document.getElementById('on').play()" 
+                : "document.getElementById('off').play()");
+        }
+        
+    }
+}
